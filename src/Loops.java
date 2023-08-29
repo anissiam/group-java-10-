b49292716d8e58dcf05264fb98390d7df151d599
anissiam <anis.siam@hotmail.com>
2023-08-29 19:00:02 +0300

    //Loop

--- src/Loops.java
+++ src/Loops.java
@@ -1,14 +1,142 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (;;){
+
+        }
+        /*for (int i = 0; i < 100; System.out.println(i)) {
+            i+=2;
+
+            *//*if (i%2==0){
+                System.out.println(i);
+            }*//*
+        }*/
+
+        /*int sum = 0;
+        for (int i = 0; i<10 ;i++) {
+            sum += i;
+        }
+        System.out.println(sum);*/
 
 
+        /*int i = 5;
+        do {
+            System.out.println("Hello");
+        } while (i != 5);*/
+
+        /*int i = 5;
+        while (i!=5){
+            System.out.println("Hello");
+
+        }*/
+
+        /*int i = 0;
+        int max = 0;
+        int min = 0;
+        while (i < 5) {
+            System.out.println("plz enter the num ");
+            int num = scanner.nextInt();
+            if (max<num){
+                max = num;
+            }
+            if (i == 0) {
+                min = num;
+            }
+
+            if (min>num){
+                min = num;
+            }
+            i++;
+        }
+        System.out.println("Max is " +max);
+        System.out.println("Min is "+ min);*/
+
+
+
+        /*System.out.println("Plz enter any word");
+        String text = scanner.nextLine();
         int i = 0;
+        while (i < text.length()) {
+            System.out.println(text.charAt(i));
+            i++;
+        }*/
+/*
+        while (true){
+            System.out.println("Enter the mark");
+            int mark = scanner.nextInt();
+            if (mark<0 || mark>100){
+                System.out.println("Not valid ");
+                continue;
+            }
+           *//* System.out.println("Enter the new mark");
+             mark = scanner.nextInt();*//*
+
+        }*/
+
+        /*int i = 1;
+        int fact = 1;
+        while (i <= 7) {
+            fact *= i;
+            i++;
+        }
+        System.out.println(fact);*/
+
+
+       /* int i = 7;
+        int fact = 1;
+        while (i>0){
+            fact *= i;
+            i--;
+        }
+
+        System.out.println("Fact is " + fact);
+*/
+
+
+        //Scanner scanner = new Scanner(System.in);
+        /*int i = 0;
+        while (i<5){
+            i++;
+            if (i==3){
+                continue;
+            }
+            System.out.println(i);
+        }*/
+
+        /*System.out.println("plz enter the text");
+        String text = scanner.nextLine();
+*/
+
+        /*while (!text.equalsIgnoreCase("stop")){
+            System.out.println("plz enter the new text");
+            text = scanner.nextLine();
+        }*/
+
+
+
+       /* while (true) {
+
+            if (text.equalsIgnoreCase("stop")) {
+                System.out.println("Stopped");
+                break;
+            }
+            System.out.println("plz enter the new text");
+            text = scanner.nextLine();
+
+        }*/
+
+
+
+        /*int i = 0;
         int sum = 0;
         while (i <= 100) {
             sum += i;
             i++;
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
 
 
         /*int i = 0;
